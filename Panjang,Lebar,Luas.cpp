b8f93266903350f573278faf9698eed09e800f55
takoyaki66 <81465823+takoyaki66@users.noreply.github.com>
2021-10-12 16:24:34 +0700

    Add files via upload

---
+++ Panjang,Lebar,Luas.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int panjang,lebar,luas;
+	
+	cout<< "masukan panjang:";
+	cin>> panjang;
+	cout<< "masukan lebar:";
+	cin>> lebar;
+	cout<<"luas =" <<panjang * lebar;
+	
+
+	
+}
